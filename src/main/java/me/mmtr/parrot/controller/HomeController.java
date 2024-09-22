5f6bf5cdd54873afc28eed6a14491e5d69190601
Velshh <pkot2003@gmail.com>
2024-09-22 20:52:09 +0200

    add a list of users on the home page

--- src/main/java/me/mmtr/parrot/controller/HomeController.java
+++ src/main/java/me/mmtr/parrot/controller/HomeController.java
@@ -1,12 +1,24 @@
 package me.mmtr.parrot.controller;
 
+import me.mmtr.parrot.service.interfaces.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class HomeController {
+    private final UserService USER_SERVICE;
+
+    public HomeController(UserService userService) {
+        this.USER_SERVICE = userService;
+    }
+
     @GetMapping("/home")
-    public String home() {
+    public String home(Model model, Principal principal) {
+        model.addAttribute("users", USER_SERVICE.findAllUsers());
+        model.addAttribute("currentUser", principal.getName());
         return "home";
     }
 }
